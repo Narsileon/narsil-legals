f3f8ef4354f238ac5af85f2dbb69c400ecd62f78
Narsilien <rigaux.jonathan@gmail.com>
2024-08-16 19:01:07 +0200

    Update packages

--- resources/js/Pages/Imprint/Index.tsx
+++ resources/js/Pages/Imprint/Index.tsx
@@ -1,6 +1,10 @@
-import { Section, SectionContent, SectionHeader, SectionTitle } from "@narsil-ui/Components";
 import { useTranslationsStore } from "@narsil-ui/Stores/translationStore";
 import parse from "html-react-parser";
+import Section from "@narsil-ui/Components/Section/Section";
+import SectionContent from "@narsil-ui/Components/Section/SectionContent";
+import SectionFullscreenToggle from "@narsil-ui/Components/Section/SectionFullscreenToggle";
+import SectionHeader from "@narsil-ui/Components/Section/SectionHeader";
+import SectionTitle from "@narsil-ui/Components/Section/SectionTitle";
 
 interface Props {
 	content: string;
@@ -13,6 +17,7 @@ const Index = ({ content }: Props) => {
 		<Section>
 			<SectionHeader>
 				<SectionTitle>{trans("Imprint")}</SectionTitle>
+				<SectionFullscreenToggle />
 			</SectionHeader>
 			<SectionContent className='prose max-w-none'>{parse(content)}</SectionContent>
 		</Section>
