084800d92256846fe47041f7440addcdaec9760c
Narsilien <rigaux.jonathan@gmail.com>
2024-08-24 07:05:25 +0200

    Export types

---
+++ resources/js/Types.ts
@@ -0,0 +1,19 @@
+export type ImprintModel = {
+	active: boolean;
+	content: string;
+	created_at: string;
+	id: number;
+	language_id: number;
+	updated_at: string;
+	language: LanguageModel;
+};
+
+export type PrivacyNoticeModel = {
+	active: boolean;
+	content: string;
+	created_at: string;
+	id: number;
+	language_id: number;
+	updated_at: string;
+	language: LanguageModel;
+};
