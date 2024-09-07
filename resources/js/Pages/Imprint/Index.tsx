998b59154fb40680d0643f83b2540117adcfd2c5
Narsilien <rigaux.jonathan@gmail.com>
2024-09-07 15:28:53 +0200

    Format files

--- resources/js/Pages/Imprint/Index.tsx
+++ resources/js/Pages/Imprint/Index.tsx
@@ -1,4 +1,5 @@
 import { useTranslationsStore } from "@narsil-localization/Stores/translationStore";
+import AppHead from "@narsil-ui/Components/App/AppHead";
 import Fullscreen from "@narsil-ui/Components/Fullscreen/Fullscreen";
 import FullscreenToggle from "@narsil-ui/Components/Fullscreen/FullscreenToggle";
 import parse from "html-react-parser";
@@ -6,7 +7,6 @@ import Section from "@narsil-ui/Components/Section/Section";
 import SectionContent from "@narsil-ui/Components/Section/SectionContent";
 import SectionHeader from "@narsil-ui/Components/Section/SectionHeader";
 import SectionTitle from "@narsil-ui/Components/Section/SectionTitle";
-import AppHead from "@narsil-ui/Components/App/AppHead";
 
 interface Props {
 	content: string;
